25b45e3ffa69b2a2b748210c1ff33333489c4b6e
Partisan Programmer <90176781+Honestflunky8@users.noreply.github.com>
2021-09-27 12:43:31 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("The time hath come!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Arise, my minions");
+            Console.WriteLine("Today is the day we'll...");
+            Console.WriteLine("...");
+            Console.WriteLine("...");
+            Console.WriteLine("To access further dialogue, please proceed to purchase the recent DLC.");
+            
         }
     }
 }
